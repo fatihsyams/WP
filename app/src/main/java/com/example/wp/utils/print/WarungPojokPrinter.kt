c9fa2ea915f7c84bac54d9999c0ed1fc959bbb53
Lianda Ramadhana <lianda.ramadhana31@gmail.com>
2020-11-14 15:55:55 +0700

    add sparato tab

--- app/src/main/java/com/example/wp/utils/print/WarungPojokPrinter.kt
+++ app/src/main/java/com/example/wp/utils/print/WarungPojokPrinter.kt
@@ -10,7 +10,6 @@ import com.example.wp.utils.lib.printerWordMng
 import com.example.wp.utils.printerFactory
 import com.example.wp.utils.toCurrencyFormat
 import com.woosim.printer.WoosimCmd
-import org.apache.http.util.ByteArrayBuffer
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -18,37 +17,48 @@ class WarungPojokPrinter(
     val context: Context,
     val order: OrderResult,
     val onPrintFinished: (() -> Unit)? = null
-) : IPrintToPrinter{
-    
+) : IPrintToPrinter {
+
     override fun printContent(prnMng: WoosimPrnMng) {
         Log.d("PRINT", "printing..")
         val wordMng: printerWordMng = printerFactory.createPaperMng(context)
         val format = SimpleDateFormat("dd-MM-yyyy 'at' HH:mm:ss", Locale.US)
         prnMng.apply {
-            printStr("Jl. Rambutan raya No. 1D RT 003/001, Kec. Pancoran Mas, Kota Depok", 1, WoosimCmd.ALIGN_CENTER)
+            printStr(
+                "Jl. Rambutan raya No. 1D RT 003/001, Kec. Pancoran Mas, Kota Depok",
+                1,
+                WoosimCmd.ALIGN_CENTER
+            )
             printStr("Tanggal: ${format.format(Date())}", 1, WoosimCmd.ALIGN_CENTER)
             printStr(
                 "Customer : ${order.order.customerName}",
                 1, WoosimCmd.ALIGN_LEFT
             )
             printStr("================================")
-            order.menu.forEach { menu->
-                printStr(wordMng.autoWordWrap("${menu.quantity} ${menu.name} \t ${toCurrencyFormat(menu.totalPrice)}"), 1, WoosimCmd.ALIGN_LEFT)
-//                printStr(toCurrencyFormat(menu.totalPrice), 1, WoosimCmd.ALIGN_RIGHT)
+            order.menu.forEach { menu ->
+                printStr(
+                    wordMng.autoWordWrap(
+                        "${menu.quantity} ${menu.name} \t ${
+                            toCurrencyFormat(
+                                menu.totalPrice
+                            )
+                        }"
+                    ), 1, WoosimCmd.ALIGN_CENTER
+                )
                 printStr(menu.additionalInformation, 1, WoosimCmd.ALIGN_LEFT)
             }
             printStr("--------------------------------", 1, WoosimCmd.ALIGN_LEFT)
-            printStr(" ITEMS: ${order.menu.size}", 1, WoosimCmd.ALIGN_LEFT)
             printStr(
-                toCurrencyFormat(order.order.totalPaymentBeforeDiscount),
+                " ITEMS: ${order.menu.size} \t ${toCurrencyFormat(order.order.totalPaymentBeforeDiscount)}",
                 1,
-                WoosimCmd.ALIGN_RIGHT
+                WoosimCmd.ALIGN_LEFT
             )
-            printStr(" Discount: ", 1, WoosimCmd.ALIGN_LEFT)
-            printStr("${order.order.discount} %", 1, WoosimCmd.ALIGN_RIGHT)
-            printStr(" Total : ", 1, WoosimCmd.ALIGN_LEFT)
-            printStr(toCurrencyFormat(order.order.totalPayment), 1, WoosimCmd.ALIGN_RIGHT)
-            printNewLine()
+            printStr(" Discount: \t ${order.order.discount} % ", 1, WoosimCmd.ALIGN_LEFT)
+            printStr(
+                " Total : \t ${toCurrencyFormat(order.order.totalPayment)} ",
+                1,
+                WoosimCmd.ALIGN_LEFT
+            )
             printStr("================================")
             printStr(printClosingMessage(order.type), 1, WoosimCmd.ALIGN_CENTER)
         }
@@ -60,8 +70,8 @@ class WarungPojokPrinter(
         onPrintFinished?.invoke()
     }
 
-    private fun printClosingMessage(orderType: Int):String{
-        return when(orderType){
+    private fun printClosingMessage(orderType: Int): String {
+        return when (orderType) {
             OrderTypeEnum.DINE_IN.type -> "Terimakasih Atas Kunjungannya"
             OrderTypeEnum.TAKE_AWAY.type -> "Terimakasih Atas Orderannya"
             OrderTypeEnum.PRE_ORDER.type -> "Selamat Menikmati"
